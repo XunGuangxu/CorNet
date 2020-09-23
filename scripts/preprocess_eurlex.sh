28fbbaccbf56b8dfb6bf1fbff0ca75d7ba28b63f
Guangxu Xun <xun.guangxu.0413@gmail.com>
2020-09-23 17:30:29 -0400

    Update preprocess_eurlex.sh

--- scripts/preprocess_eurlex.sh
+++ scripts/preprocess_eurlex.sh
@@ -4,13 +4,13 @@ DATA_PATH=/p/realai/guangxu/2020xmtc/deep_data
 DATASET=EUR-Lex
 
 python preprocess.py \
---text-path $DATA_PATH/DATASET/train_texts.txt \
---label-path $DATA_PATH/DATASET/train_labels.txt \
---vocab-path $DATA_PATH/DATASET/vocab.npy \
---emb-path $DATA_PATH/DATASET/emb_init.npy \
+--text-path $DATA_PATH/$DATASET/train_texts.txt \
+--label-path $DATA_PATH/$DATASET/train_labels.txt \
+--vocab-path $DATA_PATH/$DATASET/vocab.npy \
+--emb-path $DATA_PATH/$DATASET/emb_init.npy \
 --w2v-model $DATA_PATH/glove.840B.300d.gensim
 
 python preprocess.py \
---text-path $DATA_PATH/DATASET/test_texts.txt \
---label-path $DATA_PATH/DATASET/test_labels.txt \
---vocab-path $DATA_PATH/DATASET/vocab.npy
+--text-path $DATA_PATH/$DATASET/test_texts.txt \
+--label-path $DATA_PATH/$DATASET/test_labels.txt \
+--vocab-path $DATA_PATH/$DATASET/vocab.npy
